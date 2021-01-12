49dc30591a97477050a757eda4494f3e9bca81b7
Alexandra <alexbushueva27@gmail.com>
2021-01-12 23:27:07 +0300

    ui recoverPassword + newPassword + API

--- src/pages/NewPassword/NewPassword.tsx
+++ src/pages/NewPassword/NewPassword.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
+import SuperInputText from "../../common/c1-SuperInputText/SuperInputText";
+import SuperButton from "../../common/c2-SuperButton/SuperButton";
 
 export const NewPassword = () => {
+
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+
+    const setNewPassword = () => {
+        alert(`${password}, ${confirmPassword}`)
+    }
+
     return <div>
-        New Password
+        <h3>New Password</h3>
+        <SuperInputText type={"password"} placeholder={"password"} value={password} onChangeText={setPassword}/>
+        <div>Enter new password</div>
+
+        <SuperInputText type={"password"} placeholder={"password"} value={confirmPassword} onChangeText={setConfirmPassword} />
+        <div>Confirm new password</div>
+
+        <SuperButton onClick={setNewPassword}>Set password</SuperButton>
+
     </div>
 }
--- src/pages/RecoverPassword/RecoverPassword.tsx
+++ src/pages/RecoverPassword/RecoverPassword.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
+import SuperInputText from "../../common/c1-SuperInputText/SuperInputText";
+import SuperButton from "../../common/c2-SuperButton/SuperButton";
 
 export const RecoverPassword = () => {
+
+    const [email, setEmail] = useState("")
+
+
+    const sendEmail = () => {
+        alert(email)
+    }
+
     return <div>
-        Recover Password
+        <h3>Recover Password</h3>
+        <div>
+            <SuperInputText type={"text"} placeholder={"email"} value={email} onChangeText={setEmail} />
+            <SuperButton onClick={sendEmail}>Send email</SuperButton>
+        </div>
+        <p>On your email we'll send a link. Please click it.</p>
+
     </div>
 }
