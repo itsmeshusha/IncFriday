e3cc57d08c447fafdaeb4afae205b3779210dcbe
Alexandra <alexbushueva27@gmail.com>
2021-01-12 14:17:03 +0300

    registration is done

---
+++ src/redux/register-reducer.ts
@@ -0,0 +1,38 @@
+import {Dispatch} from "redux";
+import {registerAPI, RegisterNewUserDataType} from "../api/api-register";
+
+const initState = {
+    isRegistered: false
+}
+
+export const registerReducer = (state: InitStateType = initState, action: ActionType) => {
+    switch(action.type) {
+        case "REGISTER-REDUCER/REGISTER-NEW-USER": {
+            return {...state, isRegistered: action.isRegistered}
+        }
+        default:
+            return state
+    }
+}
+
+//AC
+export const registerNewUserAC = (isRegistered: boolean) => ({type: "REGISTER-REDUCER/REGISTER-NEW-USER", isRegistered} as const)
+
+//thunks
+export const registerNewUserTC = (newUserData: RegisterNewUserDataType) => (dispatch: Dispatch) => {
+    registerAPI.registerNewUser(newUserData)
+        .then(res => {
+            if(res.status === 201){
+                dispatch(registerNewUserAC(true))
+            } else {
+                dispatch(registerNewUserAC(false))
+                alert(res.status)
+            }
+        })
+        .catch(err => console.log(err))
+}
+
+
+//types
+type InitStateType = typeof initState
+type ActionType = ReturnType<typeof registerNewUserAC>
