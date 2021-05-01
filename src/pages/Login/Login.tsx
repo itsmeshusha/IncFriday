ba63ce67b3a931a504576501b732e275eefea864
Alexandra <alexbushueva27@gmail.com>
2021-05-01 15:30:06 +0300

    Add styles ti Login and Registration

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -4,6 +4,7 @@ import SuperCheckbox from "../../common/SuperCheckbox/SuperCheckbox";
 import SuperButton from "../../common/SuperButton/SuperButton";
 import {useDispatch, useSelector} from "react-redux";
 import {AppStoreType} from "../../redux/store";
+import s from './Login.module.css'
 
 export const Login = () => {
     const [email, setEmail] = useState("")
@@ -13,19 +14,23 @@ export const Login = () => {
     const dispatch = useDispatch()
     const isLoggedIn = useSelector<AppStoreType, boolean>(state => state.login.isLoggedIn)
 
-    return <div>
-        <div>Log In</div>
-        <SuperInputText type={"text"} placeholder={"email"} value={email} onChangeText={setEmail}/>
-        <div>Enter email</div>
-
-        <SuperInputText type={"password"} placeholder={"password"} value={password} onChangeText={setPassword}/>
-        <div>Enter password</div>
-
-        <SuperCheckbox checked={rememberMe} onChangeChecked={setRememberMe}/>
-        <span>Remember me</span>
-        <div>
-            <SuperButton>Log In</SuperButton>
+    return <div className={s.mainBlock}>
+        <div className={s.form}>
+            <div className={s.item}>
+                <div>Enter email</div>
+                <SuperInputText type={"text"} placeholder={"email"} value={email} onChangeText={setEmail}/>
+            </div>
+            <div className={s.item}>
+                <div>Enter password</div>
+                <SuperInputText type={"password"} placeholder={"password"} value={password} onChangeText={setPassword}/>
+            </div>
+            <div className={s.item}>
+                <span>Remember me</span>
+                <SuperCheckbox checked={rememberMe} onChangeChecked={setRememberMe}/>
+            </div>
+            <div className={s.item}>
+                <SuperButton>Log In</SuperButton>
+            </div>
         </div>
-
     </div>
 }
