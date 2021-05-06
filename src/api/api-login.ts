0ff67d6cf1fb1fd9bb1395ab8c38a1ca19089206
Alexandra <alexbushueva27@gmail.com>
2021-05-06 20:44:14 +0300

    Fix authMe request

--- src/api/api-login.ts
+++ src/api/api-login.ts
@@ -8,7 +8,8 @@ export type LoginDataType = {
 
 const instance = axios.create({
     // baseURL: "https://neko-back.herokuapp.com/2.0"
-    baseURL: "http://localhost:7542/2.0/" //сам сервер https://neko-back.herokuapp.com/2.0
+    baseURL: "http://localhost:7542/2.0/", //сам сервер https://neko-back.herokuapp.com/2.0
+    withCredentials: true
 })
 
 export const loginAPI = {
